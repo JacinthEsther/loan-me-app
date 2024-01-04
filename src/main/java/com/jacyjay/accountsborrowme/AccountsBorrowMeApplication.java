605c7d53a0cda07d310f03156bb25361337e8823
JacinthEsther <agbonirojacinta@gmail.com>
2024-01-04 21:16:27 +0100

    first commit

---
+++ src/main/java/com/jacyjay/accountsborrowme/AccountsBorrowMeApplication.java
@@ -0,0 +1,13 @@
+package com.jacyjay.accountsborrowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsBorrowMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsBorrowMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jacyjay/accountsborrowme/AccountsBorrowMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacyjay.accountsborrowme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsBorrowMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
